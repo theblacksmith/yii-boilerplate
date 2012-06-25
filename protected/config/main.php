76c56a881c691b18d66b5287c83c52c797246415
Saulo Vallory <me@saulovallory.com>
2012-06-25 09:02:29 -0300

    Adds the giix-haml (https://github.com/svallory/giix-haml) extension

--- protected/config/main.php
+++ protected/config/main.php
@@ -16,18 +16,22 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+		'ext.giix-haml.components.*', // giix components
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'sbrubles',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+			'generatorPaths'=>array(
+			  'ext.giix-haml.core', // giix generators
+			),
 		),
-		*/
+		
 	),
 
 	// application components
