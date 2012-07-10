fa1c11b73ddfb8e7cdeba766e6aee2f2eeae6ec4
Saulo Vallory <me@saulovallory.com>
2012-07-10 20:29:10 -0300

    Adds shortcut methods to Controller
    Adds yield(), contentFor() and endContentFor() methods which are shortcuts and adds semantics to the CClipWidget

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -9,7 +9,7 @@ class Controller extends CController
 	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
 	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
 	 */
-	public $layout='//layouts/column1';
+	public $layout='//layouts/main';
 	/**
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
 	 */
@@ -20,4 +20,27 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+	
+	public function yield($section = null, $required = false)
+	{
+		if(empty($section))
+			echo $this->content;
+		else
+		{
+			if(isset($this->clips[$section]))
+				echo $this->clips[$section];
+			else if($required)
+				throw new Exception('The layout '.$this->layout.' requires the definition of a '.$section.' section in the view.');
+		}
+	}
+	
+	public function contentFor($section)
+	{
+		$this->beginWidget('system.web.widgets.CClipWidget', array('id'=> $section));
+	}
+	
+	public function endContentFor()
+	{
+		$this->endWidget();
+	}
 }
